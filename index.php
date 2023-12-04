b72273934d32e185f36ce4eef608b1ea2e246d8d
Purna Shrestha <crush9918@gmail.com>
2023-12-05 01:21:11 +0545

    MVP Nikap Village Resort

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+$folderName = "frontend";
+
+include "{$folderName}/js/app.js";
+
+
+$template = "{$folderName}/index.html";
+
+if (file_exists($template)) {
+    include $template;
+} else {
+    echo "Template not found!";
+}
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./frontend/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+// theme: {
+//   extend: {
+//     colors: {
+//       primary: "#17202A",
+//       secondary: "#ffffff",
+//       acent: "#BD2A28",
+//       white: "#FFFFFF",
+//       pastelBlue: "#B8C4CC", // Text Color
+//       "taupeGray.": "#888888", // Border Color
+//       overlay: "rgba(0, 0, 0, 0.5)",
+//     },
+//     boxShadow: {
+//       xl: "0px 5px 20px 0px rgba(0, 0, 0, 0.20)",
+//       "2xl": "0px 0px 10px 0px rgba(0, 0, 0, 0.20) inset",
+//       "3xl": "0px 0px 20px 0px rgba(255, 255, 255, 0.20) inset",
+//     },
+//     fontSize: {
+//       xs: ["12px", "14px"],
+//       sm: ["14px", "20px"],
+//       base: ["16px", "19px"],
+//       lg: ["19px", "22px"],
+//       xl: ["23px", "28px"],
+//       "2xl": ["28px", "34px"],
+//       "3xl": ["33px", "40px"],
+//       "4xl": ["42px", "52px"],
+//     },
+//   },
+// },
