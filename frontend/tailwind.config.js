3ed9c2843bad676ef242b920f0700be869415afe
Purna Shrestha <crush9918@gmail.com>
2023-12-19 16:39:51 +0545

    Change the package dir for backend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "bg-clr": "#ef4c23",
+        "root-green-clr": "#00cb5b",
+        "root-yellow-clr": "#ffb001",
+        "light-green-clr": "#1b5045",
+        "blue-clr": "#0049df",
+        "red-clr": "#f14d2a",
+        "white-clr": "#E1F7EB",
+      },
+    },
+  },
+  plugins: [],
+};
